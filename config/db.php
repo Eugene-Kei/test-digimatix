3c1c49a2eb8db0e467830cdd51f37f397983a258
Евгений Копытов <eugene-kei@mail.ru>
2021-06-13 09:17:05 +0800

    init

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=test-digimatix-mysql;dbname=test-digimatix',
+    'username' => 'test-digimatix-user',
+    'password' => 'test-digimatix-password',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
